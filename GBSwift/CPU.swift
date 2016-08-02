b63dfb92c1ef6e58454615c2f6cd81357e11254d
Dalton Claybrook <daltonclaybrook@gmail.com>
2016-08-01 22:27:48 -0500

    Stubbing out the CPU and MMU

---
+++ GBSwift/CPU.swift
@@ -0,0 +1,45 @@
+//
+//  CPU.swift
+//  GBSwift
+//
+//  Created by Dalton Claybrook on 8/1/16.
+//  Copyright © 2016 Claybrook Software. All rights reserved.
+//
+
+struct Flags: OptionSet {
+    let rawValue: UInt8
+    
+    static let fullCarry = Flags(rawValue: 1 << 4)
+    static let halfCarry = Flags(rawValue: 1 << 5)
+    static let subtract = Flags(rawValue: 1 << 6)
+    static let zero = Flags(rawValue: 1 << 7)
+}
+
+struct Registers {
+    var a: UInt8 = 0
+    var b: UInt8 = 0
+    var c: UInt8 = 0
+    var d: UInt8 = 0
+    var e: UInt8 = 0
+    var h: UInt8 = 0
+    var l: UInt8 = 0
+    var flags: Flags = []
+    var sp: UInt16 = 0 // stack pointer
+    var pc: UInt16 = 0 // program counter
+}
+
+struct Clock {
+    var m: UInt32 = 0
+    var t: UInt32 = 0
+}
+
+struct CPU {
+    
+    var registers = Registers()
+    var clock = Clock()
+    
+    mutating func reset() {
+        registers = Registers()
+        clock = Clock()
+    }
+}
---
+++ GBSwift/MMU.swift
@@ -0,0 +1,26 @@
+//
+//  MMU.swift
+//  GBSwift
+//
+//  Created by Dalton Claybrook on 8/1/16.
+//  Copyright © 2016 Claybrook Software. All rights reserved.
+//
+
+struct MMU {
+    
+    func write(byte: UInt8, to address: UInt16) {
+        
+    }
+    
+    func write(word: UInt16, to address: UInt16) {
+        
+    }
+    
+    func readByte(address: UInt16) -> UInt8 {
+        return 0
+    }
+    
+    func readWord(address: UInt16) -> UInt16 {
+        return 0
+    }
+}
